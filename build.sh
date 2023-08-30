702ee4a9e027c47343a7c360e85dd5d602fbda65
Dora Militaru <dora@kalneus.com>
2023-08-30 23:08:51 +0100

    update build script and add wasm32-wasi config

--- build.sh
+++ build.sh
@@ -2,6 +2,15 @@
 
 NPROCESSORS=$(getconf NPROCESSORS_ONLN 2>/dev/null || getconf _NPROCESSORS_ONLN 2>/dev/null)
 
+DISABLED_WARNINGS="-Wno-unused-command-line-argument -Wno-constant-conversion -Wno-shift-count-overflow"
+
+CPP_FLAGS="-D_WASI_EMULATED_SIGNAL -D_WASI_EMULATED_PROCESS_CLOCKS -D_WASI_EMULATED_GETPID \
+    -Dgetpid=getpagesize -Dgetuid=getpagesize -Dgeteuid=getpagesize -Dgetgid=getpagesize -Dgetegid=getpagesize"
+
+LD_FLAGS="-lwasi-emulated-signal -lwasi-emulated-process-clocks -lwasi-emulated-getpid"
+
+cp configuration/50-wasm32-wasi.conf openssl/Configurations || exit 1
+
 cd openssl || exit 1
 
 env \
@@ -9,9 +18,9 @@ env \
     AR="zig ar" \
     RANLIB="zig ranlib" \
     CC="zig cc --target=wasm32-wasi" \
-    CFLAGS="-Ofast" \
-    CPPFLAGS="-DUSE_TIMEGM=1 -Dgetpid=getpagesize -Dgetuid=getpagesize -Dgeteuid=getpagesize -Dgetgid=getpagesize -Dgetegid=getpagesize" \
-    LDFLAGS="-s" \
+    CFLAGS="-Ofast ${DISABLED_WARNINGS}" \
+    CPPFLAGS="${CPP_FLAGS}" \
+    LDFLAGS="${LD_FLAGS}" \
     ./Configure \
     --banner="wasm32-wasi port" \
     no-asm \
@@ -27,7 +36,8 @@ env \
     no-thread-pool \
     no-threads \
     no-ui-console \
-    no-weak-ssl-ciphers || exit 1
+    no-weak-ssl-ciphers \
+    wasm32-wasi || exit 1
 
 make "-j${NPROCESSORS}"
 
